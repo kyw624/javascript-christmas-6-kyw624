d1e9f5ce2d0d23db6f64a637569016eb44926768
Y.W KIM <kyw624@gmail.com>
2023-11-13 00:59:43 +0900

    Feat: 특정 날짜에 적용되는 이벤트들을 구하는 getEventsByDate 메서드 구현

--- src/EventPlanner.js
+++ src/EventPlanner.js
@@ -1,8 +1,31 @@
+import { EVENTS } from './constants/events';
+
 class EventPlanner {
   constructor(date, menu) {
     this.visitDate = date;
     this.orderList = menu;
-    this.events = [];
+    this.events = null;
+    this.benefits = {};
+  }
+
+  getEventsByDate() {
+    let events = [];
+
+    if (this.visitDate <= EVENTS['D-DAY'].MAX) {
+      events.push(EVENTS['D-DAY'].NAME);
+      this.getDiscountByEvent(EVENTS['D-DAY'].NAME);
+    }
+
+    const dayType = this.getDayType();
+    events.push(EVENTS[dayType].NAME);
+    this.getDiscountByEvent(EVENTS[dayType].NAME);
+
+    if (EVENTS['SPECIAL'].DATE.includes(this.visitDate)) {
+      events.push(EVENTS['SPECIAL'].NAME);
+      this.getDiscountByEvent(EVENTS['SPECIAL'].NAME);
+    }
+
+    this.events = events || [];
   }
 }
 
