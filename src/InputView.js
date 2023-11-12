fbf876a055802ed94632b68b6425ae9504e6e5e2
Y.W KIM <kyw624@gmail.com>
2023-11-12 23:48:38 +0900

    Refactor: 방문 날짜를 입력받는 readDate 메서드 수정

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,12 @@
+import { Console } from '@woowacourse/mission-utils';
+
+import { INPUT_MESSAGE } from './constants/messages';
+
 const InputView = {
   async readDate() {
-    const input = await Console.readLineAsync(
-      '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)'
-    );
-    // ...
+    const visitDate = await Console.readLineAsync(INPUT_MESSAGE.VISIT_DATE);
+    return visitDate;
   },
-  // ...
 };
 
 export default InputView;
