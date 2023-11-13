af8bbfbc96304c5627983c803bf993d3a1482c8e
Y.W KIM <kyw624@gmail.com>
2023-11-13 17:06:53 +0900

    Feat: 할인 전 총 주문금액을 구하는 getTotalOrderAmount 메서드 구현

--- src/Order.js
+++ src/Order.js
@@ -1,10 +1,17 @@
+import { MENU } from './constants/menu.js';
+
 class Order {
   constructor(order) {
     this.orders = order;
     this.totalAmount = 0;
   }
 
-  getTotalAmount() {}
+  getTotalOrderAmount() {
+    this.orders.forEach((order) => {
+      const [food, count] = order.split('-');
+      this.totalAmount += MENU[food].price * count;
+    });
+  }
 }
 
 export default Order;
