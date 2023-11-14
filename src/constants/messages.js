f3afad1fdfbf3407ca9c30cd10ab490f0d6caa8e
Y.W KIM <kyw624@gmail.com>
2023-11-14 22:21:17 +0900

    Fix: 에러 메시지 2종 추가 및 개행 문자 변경

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,10 +1,15 @@
 import { Console } from '@woowacourse/mission-utils';
+import { EOL as LINE_SEPARATOR } from 'os';
 
 import { EVENT_BADGE, EVENT_NONE } from './events.js';
 
 export const INPUT_MESSAGE = Object.freeze({
-  VISIT_DATE: `12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n`,
-  MENU_TO_ORDER: `주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n`,
+  VISIT_DATE:
+    `12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)` +
+    LINE_SEPARATOR,
+  MENU_TO_ORDER:
+    `주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)` +
+    LINE_SEPARATOR,
 });
 
 export const RESULT_LIST = [
@@ -20,7 +25,8 @@ export const RESULT_LIST = [
 export const OUTPUT_MESSAGE = Object.freeze({
   GREETING: `안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.`,
   BENEFIT_PREVIEW: (date) =>
-    `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`,
+    `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!` +
+    LINE_SEPARATOR,
   BADGE: (price) => {
     if (price >= 20000) {
       return EVENT_BADGE.SANTA;
@@ -37,13 +43,14 @@ export const OUTPUT_MESSAGE = Object.freeze({
     return EVENT_NONE;
   },
   RESULT_DETAILS: {
-    ORDER_LIST: '<주문 메뉴>\n',
-    TOTAL_ORDER_AMOUNT: '<할인 전 총 주문 금액>\n',
-    GIFT_MENU: '\n<증정 메뉴>\n',
-    BENEFITS_LIST: '\n<혜택 내역>\n',
-    TOTAL_BENEFIT_AMOUNT: '<총 혜택 금액>\n',
-    AMOUNT_AFTER_DISCOUNT: '\n<할인 후 예상 결제 금액>\n',
-    EVENT_BADGE: '\n<12월 이벤트 배지>\n',
+    ORDER_LIST: '<주문 메뉴>' + LINE_SEPARATOR,
+    TOTAL_ORDER_AMOUNT: '<할인 전 총주문 금액>' + LINE_SEPARATOR,
+    GIFT_MENU: LINE_SEPARATOR + '<증정 메뉴>' + LINE_SEPARATOR,
+    BENEFITS_LIST: LINE_SEPARATOR + '<혜택 내역>' + LINE_SEPARATOR,
+    TOTAL_BENEFIT_AMOUNT: '<총혜택 금액>' + LINE_SEPARATOR,
+    AMOUNT_AFTER_DISCOUNT:
+      LINE_SEPARATOR + '<할인 후 예상 결제 금액>' + LINE_SEPARATOR,
+    EVENT_BADGE: LINE_SEPARATOR + '<12월 이벤트 배지>' + LINE_SEPARATOR,
   },
 });
 
@@ -52,4 +59,6 @@ const ERROR_PREFIX = '[ERROR]';
 export const ERROR_MESSAGE = Object.freeze({
   DATE: `${ERROR_PREFIX} 유효하지 않은 날짜입니다. 다시 입력해 주세요.`,
   ORDER: `${ERROR_PREFIX} 유효하지 않은 주문입니다. 다시 입력해 주세요.`,
+  MAX_ORDER_SIZE_EXCEEDED: `${ERROR_PREFIX} 메뉴는 최대 20개까지 주문 가능합니다. 다시 입력해 주세요.`,
+  ONLY_DRINK_ORDER_NOT_ALLOWED: `${ERROR_PREFIX} 음료만을 주문할 수 없습니다. 다시 입력해 주세요.`,
 });
