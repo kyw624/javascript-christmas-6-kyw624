fa33556b3d354e46bc6b60a818bfdb48087ce2f5
Y.W KIM <kyw624@gmail.com>
2023-11-14 22:22:20 +0900

    Refactor: 유효성 검사 에러 발생한 부분부터 재입력 가능하도록 개선

--- src/InputView.js
+++ src/InputView.js
@@ -1,16 +1,35 @@
 import { Console } from '@woowacourse/mission-utils';
 
 import { INPUT_MESSAGE } from './constants/messages.js';
+import { validateVisitDate, validateMenu } from './utils/validateInput.js';
 
 const InputView = {
   async readDate() {
-    const visitDate = await Console.readLineAsync(INPUT_MESSAGE.VISIT_DATE);
-    return Number(visitDate);
+    while (true) {
+      try {
+        const visitDate = await Console.readLineAsync(INPUT_MESSAGE.VISIT_DATE);
+        validateVisitDate(visitDate);
+
+        return Number(visitDate);
+      } catch (err) {
+        Console.print(err.message);
+      }
+    }
   },
 
   async readOrder() {
-    const orders = await Console.readLineAsync(INPUT_MESSAGE.MENU_TO_ORDER);
-    return orders.split(',');
+    while (true) {
+      try {
+        const orderList = await Console.readLineAsync(
+          INPUT_MESSAGE.MENU_TO_ORDER
+        );
+        validateMenu(orderList);
+
+        return orderList.split(',');
+      } catch (err) {
+        Console.print(err.message);
+      }
+    }
   },
 };
 
