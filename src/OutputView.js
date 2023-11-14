5a4e5af77323f26e71db75bda973fc923c043371
Y.W KIM <kyw624@gmail.com>
2023-11-14 17:12:05 +0900

    Feat: 이벤트 상세 내역을 출력해주는 메서드 7개 구현
    - printMenu(): 주문 메뉴들 출력
    - printTotalOrderAmount(): 할인 전 총 주문 금액 출력
    - printGift(): 증정 메뉴 출력
    - printBenefitsList(): 혜택 내역 출력
    - printTotalBenefits(): 총 혜택 금액 출력
    - printAmountAfterDiscount(): 할인 후 예상 결제 금액 출력
    - printEventBadge(): 이벤트 배지 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,9 +1,81 @@
+import { Console } from '@woowacourse/mission-utils';
+
+import { EVENT_NONE } from './constants/events.js';
+import { MENU } from './constants/menu.js';
+import { OUTPUT_MESSAGE } from './constants/messages.js';
+
+const {
+  ORDER_LIST,
+  TOTAL_ORDER_AMOUNT,
+  GIFT_MENU,
+  BENEFITS_LIST,
+  TOTAL_BENEFIT_AMOUNT,
+  AMOUNT_AFTER_DISCOUNT,
+  EVENT_BADGE,
+} = OUTPUT_MESSAGE.RESULT_DETAILS;
+
 const OutputView = {
-  printMenu() {
-    Console.print('<주문 메뉴>');
-    // ...
+  printMenu(order) {
+    let menuResult = ``;
+
+    order.forEach((menu) => {
+      const [food, count] = menu.split('-');
+      menuResult += `${food} ${count}개\n`;
+    });
+
+    Console.print(ORDER_LIST + menuResult);
   },
-  // ...
+
+  printTotalOrderAmount(totalAmount) {
+    Console.print(TOTAL_ORDER_AMOUNT + `${totalAmount.toLocaleString()}원`);
+  },
+
+  printGift(plannerInstance) {
+    const isGift = Object.keys(plannerInstance.benefits).includes('GIFT');
+    const giftResult = isGift ? '샴페인 1개' : EVENT_NONE;
+
+    Console.print(GIFT_MENU + giftResult);
+  },
+
+  printBenefitsList(plannerInstance) {
+    const benefits = Object.values(plannerInstance.benefits);
+    const isBenefits = benefits.length ? true : false;
+    const benefitsResult = isBenefits
+      ? benefits.reduce((result, { message, price }) => {
+          return result + `${message}${price.toLocaleString()}원\n`;
+        }, '')
+      : EVENT_NONE;
+
+    Console.print(BENEFITS_LIST + benefitsResult);
+  },
+
+  printTotalBenefits(plannerInstance) {
+    const benefits = Object.values(plannerInstance.benefits);
+    const isBenefits = benefits.length ? true : false;
+    const totalBenefitsResult = plannerInstance.getTotalBenefits();
+    const prefix = isBenefits ? '' : '-';
+
+    Console.print(
+      TOTAL_BENEFIT_AMOUNT +
+        `${prefix}${totalBenefitsResult.toLocaleString()}원`
+    );
+  },
+
+  printAmountAfterDiscount(orderInstance, plannerInstance) {
+    const benefits = Object.values(plannerInstance.benefits);
+    const isBenefits = benefits.length ? true : false;
+    const totalBenefits = isBenefits
+      ? plannerInstance.getTotalBenefits() - MENU['샴페인'].price
+      : 0;
+    const totalAmount = orderInstance.beforeDiscountTotalAmount;
+    const resultAmount = `${(totalAmount - totalBenefits).toLocaleString()}원`;
+
+    Console.print(AMOUNT_AFTER_DISCOUNT + resultAmount);
+  },
+
+  printEventBadge(plannerInstance) {
+    Console.print(EVENT_BADGE + plannerInstance.eventBadge);
+  },
 };
 
 export default OutputView;
