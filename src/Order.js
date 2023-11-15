1f1ce90ef7bf341e95ee0186870d28b807cbf470
Y.W KIM <kyw624@gmail.com>
2023-11-15 17:40:20 +0900

    Refactor: 외부에서 필드에 액세스 불가능하도록 캡슐화

--- src/Order.js
+++ src/Order.js
@@ -1,52 +1,59 @@
+import { EOL as LINE_SEPARATOR } from 'os';
+
 import { MENU } from './constants/menu.js';
 
 class Order {
+  #orders;
+  #beforeDiscountTotalAmount;
+  #totalDiscountAmount;
+  #finalPaymentAmount;
+
   constructor(order) {
-    this.orders = order;
-    this.beforeDiscountTotalAmount = 0;
-    this.totalDiscountAmount = 0;
-    this.finalPaymentAmount = 0;
+    this.#orders = order;
+    this.#beforeDiscountTotalAmount = 0;
+    this.#totalDiscountAmount = 0;
+    this.#finalPaymentAmount = 0;
   }
 
   getOrderList() {
-    return this.orders.reduce((orderList, order) => {
+    return this.#orders.reduce((orderList, order) => {
       const [food, count] = order.split('-');
-      return orderList + `${food} ${count}개\n`;
+      return orderList + `${food} ${count}개` + LINE_SEPARATOR;
     }, '');
   }
 
   getBeforeDiscountTotalAmount() {
-    return this.beforeDiscountTotalAmount;
+    return this.#beforeDiscountTotalAmount;
   }
 
   getTotalDiscountAmount() {
-    return this.totalDiscountAmount;
+    return this.#totalDiscountAmount;
   }
 
   getFinalPaymentAmount() {
-    return this.finalPaymentAmount;
+    return this.#finalPaymentAmount;
   }
 
   calculateBeforeDiscountTotalAmount() {
-    this.orders.forEach((order) => {
+    this.#orders.forEach((order) => {
       const [food, count] = order.split('-');
-      this.beforeDiscountTotalAmount += MENU[food].price * count;
+      this.#beforeDiscountTotalAmount += MENU[food].price * count;
     });
   }
 
   calculateTotalDiscountAmount(benefits) {
     const benefitsPrice = Object.values(benefits);
 
-    this.totalDiscountAmount = benefitsPrice.reduce(
+    this.#totalDiscountAmount = benefitsPrice.reduce(
       (total, { _, price }) => total + price,
       0
     );
   }
 
   calculateFinalPaymentAmount(isGift) {
-    this.finalPaymentAmount =
-      this.beforeDiscountTotalAmount -
-      this.totalDiscountAmount +
+    this.#finalPaymentAmount =
+      this.#beforeDiscountTotalAmount -
+      this.#totalDiscountAmount +
       (isGift ? MENU['샴페인'].price : 0);
   }
 }
